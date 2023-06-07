9334b28d81967f3fa348f3abef94273e786a5e79
Yashikab <yashikaba@gmail.com>
2023-06-07 09:03:21 +0900

    update

--- logic2/hello.py
+++ logic2/hello.py
@@ -1,4 +1,9 @@
 import module
 
+def double_element_for_dict(target: dict):
+    df = module.generate_df(target)
+    return df * 2
+
 if __name__ == "__main__":
     print(module.generate_df({"a": [1, 2], "b": [3, 4]}))
+    print(double_element_for_dict({"a": [1, 2], "b": [3, 4]}))
--- logic2/test_hello.py
+++ logic2/test_hello.py
@@ -1,8 +1,14 @@
-from logic2.hello import generate_df
-
+from module import generate_df
+from hello import double_element_for_dict
 
 def test_generate_df():
     target = {"a": [1, 2]}
     actual_df = generate_df(target)
     assert actual_df["a"][0] == 1
     assert actual_df["a"][1] == 2
+
+def test_double_df():
+    target = {"a": [1, 2]}
+    actual_df = double_element_for_dict(target)
+    assert actual_df["a"][0] == 2
+    assert actual_df["a"][1] == 4
